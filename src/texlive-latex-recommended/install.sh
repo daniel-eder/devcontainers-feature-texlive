ea90e8b779c63debbd60508283bff2c7e64febd4
Daniel Eder <danieleder@outlook.at>
2023-11-08 14:50:27 +0100

    feat: add luatex

--- src/texlive-latex-recommended/install.sh
+++ src/texlive-latex-recommended/install.sh
@@ -6,7 +6,8 @@ rm -rf /var/lib/apt/lists/*
 
 # Install
 apt-get -y update 
-apt-get -y install texlive-latex-recommended texlive-fonts-recommended texlive-latex-extra
+apt-get -y install texlive-latex-recommended texlive-fonts-recommended texlive-latex-extra texlive-luatex
+luaotfload-tool --update
 
 # Clean up
 rm -rf /var/lib/apt/lists/*
