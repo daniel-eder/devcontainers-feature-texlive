7414088a98c42b5879af1b71c48e135f0c891bd7
Daniel Eder <danieleder@outlook.at>
2023-11-08 14:47:46 +0100

    feat: more contents

--- src/texlive-latex-recommended/install.sh
+++ src/texlive-latex-recommended/install.sh
@@ -6,7 +6,7 @@ rm -rf /var/lib/apt/lists/*
 
 # Install
 apt-get -y update 
-apt-get -y install texlive-latex-recommended
+apt-get -y install texlive-latex-recommended texlive-fonts-recommended texlive-latex-extra
 
 # Clean up
 rm -rf /var/lib/apt/lists/*
